45a1e512d9e862a4dc9654b5c9f46067b9030812
Alejandro Baez <code@baez.nyc>
2015-05-19 21:02:43 -0400

    updated builtin to library, functions, constants

--- moon.lua
+++ moon.lua
@@ -41,37 +41,76 @@ local keyword = token(l.KEYWORD, word_match {
   'switch', 'unless', 'using', 'when', 'with'
 })
 
+-- Constants.
+local constant = token(l.CONSTANT, word_match{
+  '_G', '_VERSION',
+  -- Added in 5.2.
+  '_ENV'
+})
+
 -- Functions.
-local builtin = token(l.FUNCTION, word_match({
-	"_G","_VERSION","assert","collectgarbage","dofile","error","getfenv","getmetatable","ipairs","load",
-	"loadfile","loadstring","module","next","pairs","pcall","print","rawequal","rawget","rawset","require",
-	"select","setfenv","setmetatable","tonumber","tostring","type","unpack","xpcall",
+local func = token(l.FUNCTION, word_match{
+  'assert', 'collectgarbage', 'dofile', 'error', 'getmetatable', 'ipairs',
+  'load', 'loadfile', 'next', 'pairs', 'pcall', 'print', 'rawequal', 'rawget',
+  'rawset', 'require', 'select', 'setmetatable', 'tonumber', 'tostring', 'type',
+  'xpcall',
+  -- Added in 5.2.
+  'rawlen'
+})
 
-	"coroutine.create","coroutine.resume","coroutine.running","coroutine.status","coroutine.wrap","coroutine.yield",
 
-	"debug.debug","debug.getfenv","debug.gethook","debug.getinfo","debug.getlocal","debug.getmetatable",
-	"debug.getregistry","debug.getupvalue","debug.setfenv","debug.sethook","debug.setlocal","debug.setmetatable",
-	"debug.setupvalue","debug.traceback",
-
-	"io.close","io.flush","io.input","io.lines","io.open","io.output","io.popen","io.read","io.stderr","io.stdin",
-	"io.stdout","io.tmpfile","io.type","io.write",
-
-	"math.abs","math.acos","math.asin","math.atan","math.atan2","math.ceil","math.cos","math.cosh","math.deg",
-	"math.exp","math.floor","math.fmod","math.frexp","math.huge","math.ldexp","math.log","math.log10","math.max",
-	"math.min","math.modf","math.pi","math.pow","math.rad","math.random","math.randomseed","math.sin","math.sinh",
-	"math.sqrt","math.tan","math.tanh",
-
-	"os.clock","os.date","os.difftime","os.execute","os.exit","os.getenv","os.remove","os.rename","os.setlocale",
-	"os.time","os.tmpname",
-
-	"package.cpath","package.loaded","package.loaders","package.loadlib","package.path","package.preload",
-	"package.seeall",
-
-	"string.byte","string.char","string.dump","string.find","string.format","string.gmatch","string.gsub",
-	"string.len","string.lower","string.match","string.rep","string.reverse","string.sub","string.upper",
-
-	"table.concat","table.insert","table.maxn","table.remove","table.sort"
-}, "%."))
+-- Libraries.
+local library = token('library', word_match({
+  -- Coroutine.
+  'coroutine', 'coroutine.create', 'coroutine.resume', 'coroutine.running',
+  'coroutine.status', 'coroutine.wrap', 'coroutine.yield',
+  -- Coroutine added in 5.3.
+  'coroutine.isyieldable',
+  -- Module.
+  'package', 'package.cpath', 'package.loaded', 'package.loadlib',
+  'package.path', 'package.preload',
+  -- Module added in 5.2.
+  'package.config', 'package.searchers', 'package.searchpath',
+  -- UTF-8 added in 5.3.
+  'utf8', 'utf8.char', 'utf8.charpattern', 'utf8.codepoint', 'utf8.codes',
+  'utf8.len', 'utf8.offset',
+  -- String.
+  'string', 'string.byte', 'string.char', 'string.dump', 'string.find',
+  'string.format', 'string.gmatch', 'string.gsub', 'string.len', 'string.lower',
+  'string.match', 'string.rep', 'string.reverse', 'string.sub', 'string.upper',
+  -- String added in 5.3.
+  'string.pack', 'string.packsize', 'string.unpack',
+  -- Table.
+  'table', 'table.concat', 'table.insert', 'table.remove', 'table.sort',
+  -- Table added in 5.2.
+  'table.pack', 'table.unpack',
+  -- Table added in 5.3.
+  'table.move',
+  -- Math.
+  'math', 'math.abs', 'math.acos', 'math.asin', 'math.atan', 'math.ceil',
+  'math.cos', 'math.deg', 'math.exp', 'math.floor', 'math.fmod', 'math.huge',
+  'math.log', 'math.max', 'math.min', 'math.modf', 'math.pi', 'math.rad',
+  'math.random', 'math.randomseed', 'math.sin', 'math.sqrt', 'math.tan',
+  -- Math added in 5.3.
+  'math.maxinteger', 'math.mininteger', 'math.tointeger', 'math.type',
+  'math.ult',
+  -- IO.
+  'io', 'io.close', 'io.flush', 'io.input', 'io.lines', 'io.open', 'io.output',
+  'io.popen', 'io.read', 'io.stderr', 'io.stdin', 'io.stdout', 'io.tmpfile',
+  'io.type', 'io.write',
+  -- OS.
+  'os', 'os.clock', 'os.date', 'os.difftime', 'os.execute', 'os.exit',
+  'os.getenv', 'os.remove', 'os.rename', 'os.setlocale', 'os.time',
+  'os.tmpname',
+  -- Debug.
+  'debug', 'debug.debug', 'debug.gethook', 'debug.getinfo', 'debug.getlocal',
+  'debug.getmetatable', 'debug.getregistry', 'debug.getupvalue',
+  'debug.sethook', 'debug.setlocal', 'debug.setmetatable', 'debug.setupvalue',
+  'debug.traceback',
+  -- Debug added in 5.2.
+  'debug.getuservalue', 'debug.setuservalue', 'debug.upvalueid',
+  'debug.upvaluejoin',
+}, '.'))
 
 -- Identifiers.
 local identifier = token(l.IDENTIFIER, l.word)
@@ -95,7 +134,9 @@ M._rules = {
   { 'error', err },
   { 'self', self_var },
   { 'keyword', keyword },
-  { 'builtin', builtin },
+  { 'function', func},
+  { 'constant', constant},
+  { 'library', library },
   { 'identifier', proper_ident + tbl_key + identifier },
   { 'comment', comment },
   { 'number', number },
